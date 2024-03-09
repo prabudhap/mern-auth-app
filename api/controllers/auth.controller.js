704e33b831b9acbe83f48c334bb53591cde33e01
prabudhap <prabudha261@gmail.com>
2024-03-10 02:05:00 +0530

    created and added auth component backend part

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -31,10 +31,41 @@ export const signin = async(req,res,next) => {
 
         //add a token to the cookie of the browser
         const token = jwt.sign({id :validUser._id},process.env.JWT_SECRET);
+        //seperated the password
         const{ password:hashedPassword, ...rest} = validUser._doc;
-        res.cookie('access_token',token,{httpOnly : true}).status(200).json(rest)
+        //set the expiry for the token
+        const expiryDate = new Date(Date.now() + 3600000); //1 hour expiry
+        //set the cookie 
+        res.cookie('access_token',token,{httpOnly : true, expires: expiryDate}).status(200).json(rest)
 
     } catch (error) {
         next(error);
     }
 }
+
+export const google = async(req,res,next) => 
+{
+    try {
+        const user = await User.findOne({email: req.body.email})
+        
+        if (user){
+        const token = jwt.sign({id: user._id}, process.env.JWT_SECRET);
+        const{ password:hashedPassword, ...rest} = user._doc;
+        const expiryDate = new Date(Date.now() + 3600000); //1 hour expiry
+        res.cookie('access_token',token,{httpOnly : true, expires: expiryDate}).status(200).json(rest);
+        }
+        else {
+            const generatedPassword = Math.random().toString(36).slice(-8) + Math.random().toString(36).slice(-8);
+            const hashedPassword = bcryptjs.hashSync(generatedPassword,10); //gone through the 10 levels of salting
+            const newUser = new User({username:(req.body.name.split(" ").join("").toLowerCase()) + (Math.floor(Math.random() * 10000)).toString(), email: req.body.email, password: hashedPassword, profilePicture: req.body.photo});
+            await newUser.save();
+            const token = jwt.sign({id: newUser._id}, process.env.JWT_SECRET);
+            const{ password:hashedPassword2, ...rest} = newUser._doc;
+            const expiryDate = new Date(Date.now() + 3600000); //1 hour expiry
+            res.cookie('access_token',token,{httpOnly : true, expires: expiryDate}).status(200).json(rest);
+        }
+    } catch (error) {
+        next(error);
+        
+    }
+};
--- api/models/user.model.js
+++ api/models/user.model.js
@@ -15,9 +15,12 @@ const userSchema = new mongoose.Schema (
     },
     password:{
         type:String,
-        required:true,
-        
-    }
+        required:true,       
+    },
+    profilePicture:{
+        type:String,
+        default:"https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.istockphoto.com%2Fphotos%2Fprofile-image&psig=AOvVaw1OfliAT5d1BGV_JH3RSq5V&ust=1710075577317000&source=images&cd=vfe&opi=89978449&ved=0CBMQjRxqFwoTCLCNnLKe54QDFQAAAAAdAAAAABAE",
+    },
 }, 
 
 {timestamps:true} //for the creation time and the updation time of the user
